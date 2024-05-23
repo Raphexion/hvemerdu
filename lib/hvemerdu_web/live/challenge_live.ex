a386999d60c848ad957926efb52069a29b2f1443
Niklas Johansson <raphexion@gmail.com>
2024-05-23 23:35:54 +0200

    Add support to verify client page

--- lib/hvemerdu_web/live/challenge_live.ex
+++ lib/hvemerdu_web/live/challenge_live.ex
@@ -2,24 +2,80 @@ defmodule HvemerduWeb.ChallengeLive do
   @moduledoc false
   use HvemerduWeb, :live_view
 
+  alias Hvemerdu.InterCom
+  alias Phoenix.Socket.Broadcast
+
   @size 6
 
+  def size, do: @size
+
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      InterCom.subscribe()
+    end
+
     socket =
       socket
+      |> assign(:verified?, false)
       |> assign(size: @size)
       |> assign(symbols: symbols())
 
     {:ok, socket}
   end
 
+  def waiting(assigns) do
+    ~H"""
+    <div>
+      <div style="display: flex; flex-direction: row; justify-content: space-around;">
+        <div :for={symbol <- @symbols} class="text-3xl p-4 rounded-lg bg-orange-500 text-yellow-100">
+          <%= symbol %>
+        </div>
+      </div>
+      <div class="text-xl text-center p-4 text-yellow-950">
+        Den person, du taler med i telefonen, skal bekræfte denne kode.
+      </div>
+      <div class="text-xl text-center p-4 text-yellow-950">
+        Venter på bekræftelse ...
+      </div>
+      <div class="flex justify-center">
+        <span class="relative flex h-10 w-10">
+          <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-amber-400 opacity-75">
+          </span>
+          <span class="relative inline-flex rounded-full h-10 w-10 bg-amber-500"></span>
+        </span>
+      </div>
+    </div>
+    """
+  end
+
+  def success(assigns) do
+    ~H"""
+    <div class="text-3xl text-center p-4 text-yellow-950">
+      OK!
+    </div>
+    """
+  end
+
+  def handle_info(%Broadcast{event: "success"}, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_info(%Broadcast{event: "code-entered", payload: %{personal_code: symbols}}, socket) do
+    if symbols == socket.assigns.symbols do
+      InterCom.broadcast_success(symbols)
+      {:noreply, assign(socket, :verified?, true)}
+    else
+      {:noreply, socket}
+    end
+  end
+
   defp symbols do
     1..@size
     |> Enum.map(fn ii ->
       if rem(ii, 3) == 0 do
-        Enum.random(["A", "B", "C", "D", "E", "F", "G", "H"])
+        Enum.random(["B", "C", "D", "E", "F", "G", "H", "K", "L", "M", "N"])
       else
-        Enum.random(2..9)
+        Enum.random(2..9) |> Integer.to_string()
       end
     end)
   end
